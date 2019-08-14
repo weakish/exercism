728f942fd24321b6235b888267cd74691172538f
Jang Rush <weakish@gmail.com>
2019-08-14 19:51:57 +0800

    :new:solution: high-scores

---
+++ python/high-scores/high_scores.py
@@ -0,0 +1,22 @@
+# I assume scores is a list,
+# that is, the caller of these three functions will only invoke them with Iterable<Comparable>.
+
+
+def latest(scores):
+    from collections import deque
+    # assuming the last added score is appended at the end.
+    zero_or_one_element_double_ended_queue = deque(scores, maxlen=1)
+    try:
+        latest_score = zero_or_one_element_double_ended_queue.pop()
+    except IndexError:  # empty
+        return None
+    else:
+        return latest_score
+
+
+def personal_best(scores):
+    return max(scores, default=None)
+
+
+def personal_top_three(scores):
+    return sorted(scores, reverse=True)[0:3]
