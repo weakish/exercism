b093c0ae2921379d5483d8f1b6676406d8da26fb
Jang Rush <weakish@gmail.com>
2019-08-14 20:37:28 +0800

    :art:high_scores: simplify latest
    I reread the problem description,
    found out that it explicitly mentions list and tuple.
    I just read the first paragraph before.
    I chose to convert to a deque before
    because this works when score is not something sliceable.
    However, given that scores is a list or tuple,
    converting to a deque unnecessary makes code more complex
    and less readable.

--- python/high-scores/high_scores.py
+++ python/high-scores/high_scores.py
@@ -1,17 +1,13 @@
-# I assume scores is a list,
-# that is, the caller of these three functions will only invoke them with Iterable<Comparable>.
-
-
 def latest(scores):
-    from collections import deque
-    # assuming the last added score is appended at the end.
-    zero_or_one_element_double_ended_queue = deque(scores, maxlen=1)
-    try:
-        latest_score = zero_or_one_element_double_ended_queue.pop()
-    except IndexError:  # empty
-        return None
-    else:
-        return latest_score
+    # Assumes the last added score is appended at the end.
+    # Does not distinguish between:
+    #
+    # 1. a player who have not finished at least one game (scores list is empty).
+    # 2. a player got zero on the last game they played.
+    #
+    # I think this is not very rigid, but acceptable.
+    # I have seen quite a lot of games displaying zeros on score list for new players.
+    return scores[-1] if scores else 0
 
 
 def personal_best(scores):
