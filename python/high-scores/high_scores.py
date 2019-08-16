ff1801295c5c9abe3109cd44344fc74dc5ed8eb6
Jang Rush <weakish@gmail.com>
2019-08-16 20:56:55 +0800

    :art:high_scores: throw exception not return 0

--- python/high-scores/high_scores.py
+++ python/high-scores/high_scores.py
@@ -1,17 +1,25 @@
+class NotFinishOneGameYetException(Exception):
+    """Raised when player has not finished at least one game yet."""
+    pass
+
+
 def latest(scores):
     # Assumes the last added score is appended at the end.
-    # Does not distinguish between:
-    #
-    # 1. a player who have not finished at least one game (scores list is empty).
-    # 2. a player got zero on the last game they played.
-    #
-    # I think this is not very rigid, but acceptable.
-    # I have seen quite a lot of games displaying zeros on score list for new players.
-    return scores[-1] if scores else 0
+    try:
+        latest_score = scores[-1]
+    except IndexError:
+        raise NotFinishOneGameYetException()
+    else:
+        return latest_score
 
 
 def personal_best(scores):
-    return max(scores, default=0)
+    try:
+        best_score = max(scores)
+    except ValueError:
+        raise NotFinishOneGameYetException()
+    else:
+        return best_score
 
 
 def personal_top_three(scores):
