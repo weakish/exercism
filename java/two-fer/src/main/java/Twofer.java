ef7e4e86b3178ff70bd9610f93db0fae55a5a172
Jang Rush <weakish@gmail.com>
2019-08-17 18:22:52 +0800

    :fix:java:two-fer accept null para instead of no para

--- java/two-fer/src/main/java/Twofer.java
+++ java/two-fer/src/main/java/Twofer.java
@@ -1,9 +1,5 @@
 class Twofer {
-    String twofer() {
-        return "One for you, one for me.";
-    }
-
     String twofer(String name) {
-        return "One for " + name + ", one for me.";
+        return "One for " + (name == null ? "you" : name) + ", one for me.";
     }
 }
