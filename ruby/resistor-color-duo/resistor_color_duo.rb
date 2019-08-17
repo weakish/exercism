e62ee618f237f84e9980f2152d31d8017fbd9cc9
Jang Rush <weakish@gmail.com>
2019-08-17 13:46:03 +0800

    :new:ruby: resistor_color_duo

---
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -0,0 +1,16 @@
+module ResistorColorDuo
+  def self.value(first_color, second_color)
+    color_number_map = {"Black" => 0,
+      "Brown" => 1,
+      "Red" => 2,
+      "Orange" => 3,
+      "Yellow" => 4,
+      "Green" => 5,
+      "Blue" => 6,
+      "Violet" => 7,
+      "Grey" => 8,
+      "White" => 9,
+    }
+    "#{color_number_map[first_color]}#{color_number_map[second_color]}"
+  end
+end
