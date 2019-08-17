2f6d0951b9962289e657f72937d8d9faeb39448b
Jang Rush <weakish@gmail.com>
2019-08-18 01:07:48 +0800

    :new:python: hamming distance

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,2 +1,15 @@
-def distance(strand_a, strand_b):
+from functools import reduce
+
+
+class StrandLengthMismatchException(Exception):
+    """Raised when two strands length mismatched."""
     pass
+
+
+def distance(strand_a: str, strand_b: str) -> int:
+    if len(strand_a) == len(strand_b):
+        c: int = 0
+        for i in range(len(strand_a)): c = c if strand_a[i] == strand_b[i] else c + 1
+        return c
+    else:
+        raise StrandLengthMismatchException()
