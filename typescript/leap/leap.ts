a4e8522955b37a50cf4239cea0a22d04d855ad78
Jang Rush <weakish@gmail.com>
2019-08-17 17:53:01 +0800

    :art:typescript:leap: intelliJ simplifies if conditions

--- typescript/leap/leap.ts
+++ typescript/leap/leap.ts
@@ -1,11 +1,7 @@
 function isLeapYear(year: number): boolean {
     if (year % 4 == 0) {
         if (year % 100 == 0) {
-            if (year % 400 == 0) {
-                return true;
-            } else {
-                return false;
-            }
+            return year % 400 == 0;
         } else {
             return true;
         }
