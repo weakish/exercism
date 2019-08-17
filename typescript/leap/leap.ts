df3643b03eb6616a48c4af0242523fb6ff50f4a4
Jang Rush <weakish@gmail.com>
2019-08-17 17:51:44 +0800

    :new:typescript: leap year

--- typescript/leap/leap.ts
+++ typescript/leap/leap.ts
@@ -1,5 +1,17 @@
-function isLeapYear( /* Parameters go here */ ) {
-    // Your code here
+function isLeapYear(year: number): boolean {
+    if (year % 4 == 0) {
+        if (year % 100 == 0) {
+            if (year % 400 == 0) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return true;
+        }
+    } else {
+        return false;
+    }
 }
 
 export default isLeapYear
