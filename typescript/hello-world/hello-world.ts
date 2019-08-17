29f4cc3d8cfeca3fa9225308a393473aab5cfa09
Jang Rush <weakish@gmail.com>
2019-08-17 17:34:59 +0800

    :new:typescript: hello world

--- typescript/hello-world/hello-world.ts
+++ typescript/hello-world/hello-world.ts
@@ -1,6 +1,6 @@
 class HelloWorld {
-    static hello( /* Parameters go here */ ) {
-        // Your code here
+    static hello(name: string = "World") {
+        return `Hello, ${name}!`
     }
 }
 
