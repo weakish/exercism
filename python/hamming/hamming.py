e1ec0908f41b9821436d11834348ebf0df93e9f7
Jang Rush <weakish@gmail.com>
2019-08-18 01:10:34 +0800

    :bug:python:hamming raise ValueError instead
    tests do not expect custom exception

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,15 +1,10 @@
 from functools import reduce
 
 
-class StrandLengthMismatchException(Exception):
-    """Raised when two strands length mismatched."""
-    pass
-
-
 def distance(strand_a: str, strand_b: str) -> int:
     if len(strand_a) == len(strand_b):
         c: int = 0
         for i in range(len(strand_a)): c = c if strand_a[i] == strand_b[i] else c + 1
         return c
     else:
-        raise StrandLengthMismatchException()
+        raise ValueError("two strands length mismatch")
