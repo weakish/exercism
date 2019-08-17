34b25c3615c9161696ccf61ecf1226b4b90ff724
Jang Rush <weakish@gmail.com>
2019-08-17 18:43:57 +0800

    :art:c#:leap one liner

--- csharp/leap/Leap.cs
+++ csharp/leap/Leap.cs
@@ -4,27 +4,6 @@ public static class Leap
 {
     public static bool IsLeapYear(int year)
     {
-        if (year % 4 == 0)
-        {
-            if (year % 100 == 0)
-            {
-                if (year % 400 == 0)
-                {
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
-            }
-            else
-            {
-                return true;
-            }
-        }
-        else
-        {
-            return false;
-        }
+        return year % 400 == 0 || (year % 4 == 0 && year % 100 != 0);
     }
 }
