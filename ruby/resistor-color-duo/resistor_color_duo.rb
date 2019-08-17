126990d7cb16894d82818038815f068efec098df
Jang Rush <weakish@gmail.com>
2019-08-17 13:55:16 +0800

    :bug:ruby:resistor-color-duo lowercase color name

--- ruby/resistor-color-duo/resistor_color_duo.rb
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -1,16 +1,16 @@
 module ResistorColorDuo
   def self.value(colors)
     first_color, second_color = colors
-    color_number_map = {"Black" => 0,
-      "Brown" => 1,
-      "Red" => 2,
-      "Orange" => 3,
-      "Yellow" => 4,
-      "Green" => 5,
-      "Blue" => 6,
-      "Violet" => 7,
-      "Grey" => 8,
-      "White" => 9,
+    color_number_map = {"black" => 0,
+      "brown" => 1,
+      "red" => 2,
+      "orange" => 3,
+      "yellow" => 4,
+      "green" => 5,
+      "blue" => 6,
+      "violet" => 7,
+      "grey" => 8,
+      "white" => 9,
     }
     "#{color_number_map[first_color]}#{color_number_map[second_color]}"
   end
