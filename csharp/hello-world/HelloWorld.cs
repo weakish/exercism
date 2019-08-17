68c037fae9ebcf11a41d157d1c759fbd42ef81a3
Jang Rush <weakish@gmail.com>
2019-08-17 16:58:20 +0800

    :new:c#: hello world

--- csharp/hello-world/HelloWorld.cs
+++ csharp/hello-world/HelloWorld.cs
@@ -4,6 +4,6 @@ public static class HelloWorld
 {
     public static string Hello()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return "Hello, World!";
     }
 }
