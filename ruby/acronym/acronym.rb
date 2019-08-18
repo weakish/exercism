48528a5b1430a115e85bb9b0b2fd18c419bf8fa1
Jang Rush <weakish@gmail.com>
2019-08-18 21:42:38 +0800

    :bug:ruby:acronym specially treat dash

--- ruby/acronym/acronym.rb
+++ ruby/acronym/acronym.rb
@@ -1,5 +1,5 @@
 module Acronym
   def self.abbreviate(full_name)
-    full_name.split.map { |word| word[0].capitalize }.join
+    full_name.sub(/-/, " ").split.map { |word| word[0].capitalize }.join
   end
 end
