0edb45d080628bdcb9e5c4df7d4bb3910a9067cc
Mario <s.mario95@googlemail.com>
2019-08-18 18:27:30 +0800

    :art:rust:gigasecond use Duration from chrono

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -1,5 +1,4 @@
-use chrono::{DateTime, Utc};
-use time::Duration;
+use chrono::{DateTime, Duration, Utc};
 
 // Returns a Utc DateTime one billion seconds after start.
 pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
