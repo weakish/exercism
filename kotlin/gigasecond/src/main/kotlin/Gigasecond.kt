ab4bbdd7880b709b4780d7bb47db76e4e2a56546
Rubén Díaz Martínez <rubendm92@gmail.com>
2019-08-19 02:43:21 +0800

    :art:kotlin:gigasecond prettier syntax

--- kotlin/gigasecond/src/main/kotlin/Gigasecond.kt
+++ kotlin/gigasecond/src/main/kotlin/Gigasecond.kt
@@ -2,9 +2,9 @@ import java.time.Duration
 import java.time.LocalDate
 import java.time.LocalDateTime
 
-const val GIGA: Long = 1000000000
+const val GIGA: Long = 1_000_000_000
 
 class Gigasecond(datetime: LocalDateTime) {
     val date: LocalDateTime = datetime + Duration.ofSeconds(GIGA)
-    constructor(date: LocalDate) : this(date.atStartOfDay()) {}
+    constructor(date: LocalDate) : this(date.atStartOfDay())
 }
