32b399379d8188f4e23aa286f702e4c835dfe42b
Jang Rush <weakish@gmail.com>
2019-08-18 18:54:58 +0800

    :art:ruby:resistor-color-duo use constant
    as suggented by programatologist

--- ruby/resistor-color-duo/resistor_color_duo.rb
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -1,17 +1,18 @@
 module ResistorColorDuo
+  COLOR_NUMBER_MAP = {"black" => 0,
+    "brown" => 1,
+    "red" => 2,
+    "orange" => 3,
+    "yellow" => 4,
+    "green" => 5,
+    "blue" => 6,
+    "violet" => 7,
+    "grey" => 8,
+    "white" => 9,
+  }
+
   def self.value(colors)
     first_color, second_color = colors
-    color_number_map = {"black" => 0,
-      "brown" => 1,
-      "red" => 2,
-      "orange" => 3,
-      "yellow" => 4,
-      "green" => 5,
-      "blue" => 6,
-      "violet" => 7,
-      "grey" => 8,
-      "white" => 9,
-    }
-    Integer("#{color_number_map[first_color]}#{color_number_map[second_color]}")
+    Integer("#{COLOR_NUMBER_MAP[first_color]}#{COLOR_NUMBER_MAP[second_color]}")
   end
 end
