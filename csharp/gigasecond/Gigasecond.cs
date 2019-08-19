8e20704b42d211caddd85b98794c95a16a3b5400
Jang Rush <weakish@gmail.com>
2019-08-19 20:34:03 +0800

    :art:csharp:gigsecond use const as suggested by PetruF

--- csharp/gigasecond/Gigasecond.cs
+++ csharp/gigasecond/Gigasecond.cs
@@ -2,8 +2,9 @@ using System;
 
 public static class Gigasecond
 {
+    private const int Giga = 1_000_000_000;
     public static DateTime Add(DateTime moment)
     {
-        return moment + new TimeSpan(0,0,1000000000);
+        return moment + new TimeSpan(0,0,Giga);
     }
 }
