80d1ce11c63e1ae30055f01d040a65c03e412672
Jang Rush <weakish@gmail.com>
2019-08-19 20:50:27 +0800

    :fix:ruby:acronym more general solution
    Previous it only covers space and dash seperated words.
    Now it covers more delimiters.
    
    Thanks to pgaspar for their hint.

--- ruby/acronym/acronym.rb
+++ ruby/acronym/acronym.rb
@@ -1,5 +1,5 @@
 module Acronym
   def self.abbreviate(full_name)
-    full_name.sub(/-/, " ").split.map { |word| word[0].capitalize }.join
+    full_name.split(/\W+/).map { |word| word[0].capitalize }.join
   end
 end
