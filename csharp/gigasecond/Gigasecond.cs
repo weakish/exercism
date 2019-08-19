7ba90d5c69237a57e461ea6b27d935436540683e
Jang Rush <weakish@gmail.com>
2019-08-19 20:38:02 +0800

    :art:csharp:gigasecond use AddSeconds as suggested by PetruF

--- csharp/gigasecond/Gigasecond.cs
+++ csharp/gigasecond/Gigasecond.cs
@@ -5,6 +5,6 @@ public static class Gigasecond
     private const int Giga = 1_000_000_000;
     public static DateTime Add(DateTime moment)
     {
-        return moment + new TimeSpan(0,0,Giga);
+        return moment.AddSeconds(Giga);
     }
 }
