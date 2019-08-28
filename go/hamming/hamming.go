02ceec4add8157451ee333f3541db257b3a3ec91
Jang Rush <weakish@gmail.com>
2019-08-28 21:57:31 +0800

    :art:go:hamming refactor according to golint

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -2,16 +2,16 @@ package hamming
 
 import "errors"
 
+// Distance calculates the hamming distance of two ascii strings.
 func Distance(a, b string) (int, error) {
 	if len(a) != len(b) {
 		return 0, errors.New("length of sequences mismatch")
-	} else {
-		var c int = 0
-		for i := 0; i < len(a); i++ {
-			if a[i] != b[i] {
-				c++
-			}
-		}
-		return c, nil
 	}
+	var c int
+	for i := 0; i < len(a); i++ {
+		if a[i] != b[i] {
+			c++
+		}
+	}
+	return c, nil
 }
