140cdc7b478c60025ba5eec0670ef2df09fca144
Jang Rush <weakish@gmail.com>
2019-08-29 21:28:27 +0800

    :art:typescript:leap: one liner

--- typescript/leap/leap.ts
+++ typescript/leap/leap.ts
@@ -1,13 +1,5 @@
 function isLeapYear(year: number): boolean {
-    if (year % 4 == 0) {
-        if (year % 100 == 0) {
-            return year % 400 == 0;
-        } else {
-            return true;
-        }
-    } else {
-        return false;
-    }
+    return year % 400 == 0 || (year % 4 == 0 && year % 100 != 0);
 }
 
 export default isLeapYear
