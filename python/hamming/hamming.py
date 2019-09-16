440240f7e842a8b60139653a0af919c7f1b40cd5
Jang Rush <weakish@gmail.com>
2019-09-16 20:18:39 +0800

    :art:python:hamming style tweaks
    As suggested by cspollar:
    
    - Flat is better than nested.
    - Limit all lines to a maximum of 79 characters.

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -2,9 +2,10 @@ from functools import reduce
 
 
 def distance(strand_a: str, strand_b: str) -> int:
-    if len(strand_a) == len(strand_b):
-        c: int = 0
-        for i in range(len(strand_a)): c = c if strand_a[i] == strand_b[i] else c + 1
-        return c
-    else:
+    if len(strand_a) != len(strand_b):
         raise ValueError("two strands length mismatch")
+        
+    c: int = 0
+    for i in range(len(strand_a)):
+        c = c if strand_a[i] == strand_b[i] else c + 1
+    return c
